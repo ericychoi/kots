3b4e537cca23e3e6ce9623c1c461d487b1db8128
Eric Choi <eric.yongjun.choi@gmail.com>
2017-01-03 00:39:39 -0800

    updated list

--- kots.pl
+++ kots.pl
@@ -14,34 +14,36 @@ use Getopt::Long;
 
 # midnight = PST 7am
 my $date = strftime('%y%m%d', localtime);
-my $host = '192.168.2.100:9091';
+my $host = '192.168.1.2:9091';
 
 GetOptions( 'date=s' => \$date );
 
-run_tranmission('무한도전', "./kots -regex '무한도전\\..+\\.$date\\.HDTV\\.H264\\.720p-\\w+' -show '무한도전'");
+run_tranmission('무한도전');
 run_tranmission('런닝맨');
+run_tranmission('아는 형님');
 run_tranmission('냉장고를');
-run_tranmission('비정상회담');
+#run_tranmission('비정상회담');
 run_tranmission('너의 목소리가');
-run_tranmission('개그콘서트');
+#run_tranmission('개그콘서트');
 run_tranmission('마이 리틀 텔레비전');
-run_tranmission('문제적 남자');
 run_tranmission('꽃보다 청춘');
-run_tranmission('님과 함께');
-run_tranmission('SNL코리아');
+run_tranmission('최고의 사랑');
+run_tranmission('삼시세끼');
+run_tranmission('SNL 코리아');
 run_tranmission('신서유기');
 run_tranmission('SHOW ME THE MONEY');
 run_tranmission('노래의 탄생', "./kots -regex '노래의 탄생.+\\.$date\\.720p-NEXT' -show '노래의 탄생'");
-run_tranmission('유희열의 스케치북', "./kots -regex '유희열의 스케치북.+\\.$date\\.720p-NEXT' -show '유희열의 스케치북'");
-run_tranmission('예림이네 만물트럭');
-run_tranmission('1박2일');
-run_tranmission('진짜 사나이');
-run_tranmission('정글의 법칙', "./kots -regex '정글의 법칙.+\\.$date\\.720p-NEXT' -show '정글의 법칙'");
-run_tranmission('슈가맨', "./kots -regex '슈가맨.+\\.$date\\.720p-NEXT' -show '슈가맨'");
+#run_tranmission('진짜 사나이');
+run_tranmission('정글의 법칙', "./kots -regex '정글의 법칙.+\\.$date\\..+720p-NEXT' -show '정글의 법칙'");
+run_tranmission('해피 투게더');
+#run_tranmission('언프리티 랩스타');
+run_tranmission('슬램덩크');
+run_tranmission('미운 우리 새끼');
+run_tranmission('K팝스타');
 
 sub run_tranmission {
   my ($show, $cmd) = @_;
-  my $magnet_cmd = $cmd || "./kots -regex '$show.+\\.$date\\.720p-NEXT' -show $show";
+  my $magnet_cmd = $cmd || "./kots -regex '$show.+\\.$date\\..*720p-NEXT' -show $show";
   my $encoded_cmd = encode("utf8", $magnet_cmd);
   print +localtime().": running $encoded_cmd\n";
   my $magnet_link = `$encoded_cmd`;
